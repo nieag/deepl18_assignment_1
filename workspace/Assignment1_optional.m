6fd6e3fd96f3f68988bed7e9a1505275d4553f7c
Niels Agerskov <agerskov@kth.se>
2018-04-11 14:47:37 +0200

    Assignment 1 finished

--- workspace/Assignment1_optional.m
+++ workspace/Assignment1_optional.m
@@ -1,11 +1,11 @@
 clc, clear all;
 rng(400);
-%%% Load data
+%%% Load data for gradient evaluation
 % [trainX, trainY, trainy] = LoadBatch('Dataset/data_batch_1.mat');
 % [valX, valY, valy] = LoadBatch('Dataset/data_batch_5.mat');
-[testX, testY, testy] = LoadBatch('Dataset/test_batch.mat');
+% [testX, testY, testy] = LoadBatch('Dataset/test_batch.mat');
 
-%%% Use all data sets
+%%% Load all data sets
 [tx1, tY1, ty1] = LoadBatch('Dataset/data_batch_1.mat');
 [tx2, tY2, ty2] = LoadBatch('Dataset/data_batch_2.mat');
 [tx3, tY3, ty3] = LoadBatch('Dataset/data_batch_3.mat');
@@ -26,17 +26,15 @@ y_valid = ty5(:,9001:10000);
 [K, ~] = size(Y_train);
 W = 0.01*randn(K, d);
 b = 0.01*randn(K,1);
-lambda = 0.1;
-GDparams.loss = 'crossEnt';
-% J1 = ComputeCost(trainX, trainY, W, b, lambda, GDparams)
+% lambda = 0.1;
 
 
 %%% Gradient evaluation
 % P = EvaluateClassifier(trainX(:,1), W, b);
 % 
-% [ngrad_b_slow, ngrad_W_slow] = ComputeGradsNumSlow(trainX(:,1), trainY(:,1), W, b, lambda, 1e-6, GDparams);
-% [ngrad_b_quick, ngrad_W_quick] = ComputeGradsNum(trainX(:,1), trainY(:, 1), W, b, lambda, 1e-6, GDparams);
-% [grad_W, grad_b] = ComputeGradients(trainX(:, 1), trainY(:, 1), P, W, lambda, GDparams);
+% [ngrad_b_slow, ngrad_W_slow] = ComputeGradsNumSlow(trainX(:,1), trainY(:,1), W, b, lambda, 1e-6);
+% [ngrad_b_quick, ngrad_W_quick] = ComputeGradsNum(trainX(:,1), trainY(:, 1), W, b, lambda, 1e-6);
+% [grad_W, grad_b] = ComputeGradients(trainX(:, 1), trainY(:, 1), P, W, lambda);
 % 
 % eps = 1e-6;
 % diff_W_slow = CompareGrads(grad_W, ngrad_W_slow)
@@ -46,11 +44,11 @@ GDparams.loss = 'crossEnt';
 % diff_b_quick = CompareGrads(grad_b, ngrad_b_quick)
 
 %%% Train and evaluate network on test data
-lambda = 0.01;
-GDparams.n_batch=100;
-GDparams.eta=0.02;
-GDparams.n_epochs = 200;
-GDparams.decay = 0.9;
+lambda = 0.01; % Regularisation
+GDparams.n_batch=100; % Batch size
+GDparams.eta=0.02; % Learning rate
+GDparams.n_epochs = 200; % Number of epochs to train
+GDparams.decay = 0.9; % Decay rate for the learning rate
 
 
 [Wstar, bstar, trainLoss, valLoss] = MiniBatchGD(X_train, Y_train, X_valid, Y_valid, GDparams, W, b, lambda);
@@ -96,16 +94,11 @@ sum = bsxfun(@plus, W*X, b);
 P = softmax(sum);
 end
 
-function J = ComputeCost(X, Y, W, b, lambda, GDparams)
+function J = ComputeCost(X, Y, W, b, lambda)
 P = EvaluateClassifier(X, W, b);
 D = size(X, 2);
 Wij = sum(sum(W.^2,1),2);
-if strcmp(GDparams.loss, 'crossEnt')
-    loss = -log(sum(Y.*P));
-elseif strcmp(GDparams.loss, 'SVM')
-    temp = max(0, P - P.*Y + 1);
-    loss = sum(temp)-1;
-end
+loss = -log(sum(Y.*P));
 J = (1/D)*sum(loss)+lambda*Wij;
 end
 
@@ -157,12 +150,12 @@ for i=1:n_epochs
         W = W - eta*grad_W;
         b = b - eta*grad_b;
     end
-    if mod(i,10)==0
-    eta = eta * decay;    
+    if mod(i,10)==0 % Decay the learning rate by factor "decay" every 10 epochs
+        eta = eta * decay;    
     end
-    trainLoss = ComputeCost(trainX, trainY, W, b, lambda, GDparams);
-    valLoss = ComputeCost(valX, valY, W, b, lambda, GDparams)
-    if valLoss<=valLossOld
+    trainLoss = ComputeCost(trainX, trainY, W, b, lambda);
+    valLoss = ComputeCost(valX, valY, W, b, lambda)
+    if valLoss<=valLossOld % Save the model that minimises the validation loss to avoid overfitting to training data
        Wstar = W;
        bstar = b;
     end
@@ -170,14 +163,11 @@ for i=1:n_epochs
     vL_saved = [vL_saved; valLoss];
     valLossOld=valLoss;
 end
-
-% Wstar = W;
-% bstar = b;
 end
 
 %numerical gradients
 %slow
-function [grad_b, grad_W] = ComputeGradsNumSlow(X, Y, W, b, lambda, h, GDparams)
+function [grad_b, grad_W] = ComputeGradsNumSlow(X, Y, W, b, lambda, h)
 
 no = size(W, 1);
 d = size(X, 1);
@@ -188,10 +178,10 @@ grad_b = zeros(no, 1);
 for i=1:length(b)
     b_try = b;
     b_try(i) = b_try(i) - h;
-    c1 = ComputeCost(X, Y, W, b_try, lambda, GDparams);
+    c1 = ComputeCost(X, Y, W, b_try, lambda);
     b_try = b;
     b_try(i) = b_try(i) + h;
-    c2 = ComputeCost(X, Y, W, b_try, lambda, GDparams);
+    c2 = ComputeCost(X, Y, W, b_try, lambda);
     grad_b(i) = (c2-c1) / (2*h);
 end
 
@@ -199,18 +189,18 @@ for i=1:numel(W)
     
     W_try = W;
     W_try(i) = W_try(i) - h;
-    c1 = ComputeCost(X, Y, W_try, b, lambda, GDparams);
+    c1 = ComputeCost(X, Y, W_try, b, lambda);
     
     W_try = W;
     W_try(i) = W_try(i) + h;
-    c2 = ComputeCost(X, Y, W_try, b, lambda, GDparams);
+    c2 = ComputeCost(X, Y, W_try, b, lambda);
     
     grad_W(i) = (c2-c1) / (2*h);
 end
 end
 
 %quick
-function [grad_b, grad_W] = ComputeGradsNum(X, Y, W, b, lambda, h, GDparams)
+function [grad_b, grad_W] = ComputeGradsNum(X, Y, W, b, lambda, h)
 
 no = size(W, 1);
 d = size(X, 1);
@@ -218,12 +208,12 @@ d = size(X, 1);
 grad_W = zeros(size(W));
 grad_b = zeros(no, 1);
 
-c = ComputeCost(X, Y, W, b, lambda, GDparams);
+c = ComputeCost(X, Y, W, b, lambda);
 
 for i=1:length(b)
     b_try = b;
     b_try(i) = b_try(i) + h;
-    c2 = ComputeCost(X, Y, W, b_try, lambda, GDparams);
+    c2 = ComputeCost(X, Y, W, b_try, lambda);
     grad_b(i) = (c2-c) / h;
 end
 
@@ -231,7 +221,7 @@ for i=1:numel(W)
     
     W_try = W;
     W_try(i) = W_try(i) + h;
-    c2 = ComputeCost(X, Y, W_try, b, lambda, GDparams);
+    c2 = ComputeCost(X, Y, W_try, b, lambda);
     
     grad_W(i) = (c2-c) / h;
 end
